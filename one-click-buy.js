e14e246723cf05f0ebc3af65c52b6c199b78fc83
Shane <contact@shanemcgowan.me>
2019-05-21 21:25:03 +0100

    sidebar now being appended to side instead of adding to body

--- one-click-buy.js
+++ one-click-buy.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Torn - Item market quick buy
-// @namespace    http://tampermonkey.net/
+// @namespace    CoolBoiIndustries
 // @version      0.1
 // @description  one click buy™
 // @author       You
@@ -12,14 +12,6 @@
   'use strict';
 
   setTimeout(function () {
-    const items = document.getElementsByClassName('buy-link');
-
-    const itemIds = [];
-    for (const item of items) {
-      const id = item.getAttribute('data-id');
-      itemIds.push(id);
-    }
-
     // https://www.torn.com/imarket.php
     // step=buyItemConfirm&ID=%%ID_GOES_HERE%%&item=0
     const html = `
@@ -52,45 +44,60 @@
     
         </div>`;
 
-    const body = document.getElementsByTagName('body')[0];
-    body.innerHTML += html;
-    const area = document.getElementById('itemMarketQuickBuyOutput');
+    const sidebar = document.createElement('div');
+    sidebar.innerHTML = html;
+    sidebar.setAttribute('id', 'itemMarketQuickBuyContainer');
+    document.body.appendChild(sidebar);
 
-    itemIds.forEach(id => {
-      const buttonId = 'itemMarketQuickBuyOutputBtn' + id;
-      area.innerHTML += `<button class="itemMarketQuickBuyButton" id="${buttonId}">${id}</button>`;
-    });
-
-    itemIds.forEach(id => {
-      const buttonId = 'itemMarketQuickBuyOutputBtn' + id;
-      document.getElementById(buttonId).addEventListener('click', function () {
-        const url = "https://www.torn.com/imarket.php";
-        const formData = 'step=buyItemConfirm&ID=' + id + '&item=0'
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", url, false);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-              // mark complete
-              document.getElementById(buttonId).style = 'display:none;'
-              const index = itemIds.indexOf(id);
-              if (index > -1) {
-                itemIds.splice(index, 1);
-              }
-
-              if(itemIds.length === 0){
-                window.location.reload();
-              }
-            } else {
-              alert('error');
-            }
-          }
-        }
-        xhr.send(formData);
-      });
-    });
+    init();
 
   }, 1000);
 })();
+
+function init() {
+  const area = document.getElementById('itemMarketQuickBuyOutput');
+  const items = document.getElementsByClassName('buy-link');
+
+  const itemIds = [];
+  for (const item of items) {
+    const id = item.getAttribute('data-id');
+    itemIds.push(id);
+  }
+
+  itemIds.forEach(id => {
+    const buttonId = 'itemMarketQuickBuyOutputBtn' + id;
+    area.innerHTML += `<button class="itemMarketQuickBuyButton" id="${buttonId}">${id}</button>`;
+  });
+
+  itemIds.forEach(id => {
+    const buttonId = 'itemMarketQuickBuyOutputBtn' + id;
+    document.getElementById(buttonId).addEventListener('click', function () {
+      const url = "https://www.torn.com/imarket.php";
+      const formData = 'step=buyItemConfirm&ID=' + id + '&item=0'
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", url, false);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+      xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            // mark complete
+            document.getElementById(buttonId).style = 'display:none;'
+            const index = itemIds.indexOf(id);
+            if (index > -1) {
+              itemIds.splice(index, 1);
+            }
+
+            if (itemIds.length === 0) {
+              window.location.reload();
+            }
+          } else {
+            alert('error');
+          }
+        }
+      }
+      xhr.send(formData);
+    });
+  });
+
+}
